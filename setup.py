52445b2190539d6f77300f3704dc623ff1c19f08
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2019-01-18 14:12:58 +0100

    Added dill to dependencies.

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     description="Bidirectional Long-Short Term Memory sequence tagger.",
     url="https://github.com/bplank/bilstm-aux",
     packages=['src'],
-    install_requires=['dyNET>=2.1', 'numpy>=1.16.0', 'scipy>=1.2.0'],
+    install_requires=['dyNET>=2.1', 'numpy>=1.16.0', 'scipy>=1.2.0', 'dill>=0.2.8.2'],
     long_description=read('README.md'),
 )
