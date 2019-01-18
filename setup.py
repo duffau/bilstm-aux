8c3137e0a358365fab81942cf2a9eafd4da58984
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2019-01-18 14:10:53 +0100

    Added a setuptools setup-file

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="bilstm-aux",
+    version="1.0.0",
+    description="Bidirectional Long-Short Term Memory sequence tagger.",
+    url="https://github.com/bplank/bilstm-aux",
+    packages=['src'],
+    install_requires=['dyNET>=2.1', 'numpy>=1.16.0', 'scipy>=1.2.0'],
+    long_description=read('README.md'),
+)
